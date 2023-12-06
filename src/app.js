c57c895411431987d0c0f35c2876d494a300b27d
Carlos Perren <idux.net@gmail.com>
2023-12-06 13:10:00 -0300

    Listado de tareas por día, semana, mes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
 import tasksRouter from './routes/tasks.routes.js'
 
 const APP_PORT = 5050
+const MONGOOSE_URL = 'mongodb://127.0.0.1:27017/rolling64i'
 
 try {
+    await mongoose.connect(MONGOOSE_URL)
+
     const app = express()
     
     app.listen(APP_PORT, () => {
-        console.log(`Backend activo puerto ${APP_PORT}`)
+        console.log(`Backend activo puerto ${APP_PORT}, conectado a bbdd`)
     })
 
     app.use(express.json())
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,38 @@
 import { Router } from 'express'
+import taskModel from '../models/task.model.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.status(200).send({ status: 'OK', data: 'LISTADO DE TAREAS' })
+router.get('/:period?', async (req, res) => {
+    const current = new Date()
+    const dateStart = new Date(current)
+    const dateEnd = new Date(current)
+
+    if (!req.params.period) req.params.period = 'week'
+
+    dateStart.setHours(0, 0, 0, 0)
+    switch (req.params.period) {
+        case 'day':
+            dateEnd.setHours(23, 59, 59, 999)
+            break
+        
+        case 'week':
+            dateEnd.setDate(current.getDate() + (6 - current.getDay()))
+            break
+        
+        case 'month':
+            dateEnd.setDate(current.getDate() + (31 - current.getDay()))
+        
+        default:
+            dateEnd.setDate(current.getDate() + (6 - current.getDay()))
+    }
+    
+    const tasks = await taskModel.aggregate([
+        { $match: { target_date: { $gte: dateStart, $lte: dateEnd }}},
+        { $sort: { target_date: 1 }}
+    ])
+
+    res.status(200).send({ status: 'OK', data: tasks })
 })
 
 export default router
